4e02d4b18a9c79807ff09a057c449dea91828ff6
Sam <9093730+sclaney@users.noreply.github.com>
2020-04-20 15:25:28 -0500

    added php for handle socialEntryForm data

---
+++ scripts/getFormInfo.php
@@ -0,0 +1,17 @@
+<html>
+  <body>
+
+    <h1>Example 3-1</h1>
+    
+    <p>here's a record of what information you submitted:</p>
+
+    <p>
+      First name: <?php echo $_REQUEST['first_name']; ?><br />
+      Last name: <?php echo $_REQUEST['last_name']; ?><br />
+      Email: <?php echo $_REQUEST['email']; ?><br />
+      Facebook URL: <?php echo $_REQUEST['facebook_url']; ?><br />
+      Twitter Handle: <?php echo $_REQUEST['twitter_handle']; ?><br />
+    </p>
+
+  </body>
+</html>
