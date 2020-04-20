00d5c26bcb838e0e67ac5fdcde1b671f653dbca7
Sam <9093730+sclaney@users.noreply.github.com>
2020-04-20 18:44:19 -0500

    added logic to check link validity

--- scripts/getFormInfo.php
+++ scripts/getFormInfo.php
@@ -3,6 +3,10 @@
   $last_name = $_REQUEST['last_name'];
   $email = $_REQUEST['email'];
   $facebook_url = $_REQUEST['facebook_url'];
+  $position = strpos($facebook_url, "facebook.com");
+  if ($position === false) {
+    $facebook_url = "https://facebook.com/" . $facebook_url;
+  }
   $twitter_handle = $_REQUEST['twitter_handle'];
 ?>
 
@@ -16,7 +20,7 @@
     <p>
       Name: <?php echo $first_name . " " . $last_name; ?><br />
       Email: <?php echo $email; ?><br />
-      Facebook URL: <?php echo $facebook_url; ?><br />
+      <a href = "<?php echo $facebook_url; ?>">Your Facebook Page</a><br />
       Twitter Handle: <?php echo $twitter_handle; ?><br />
     </p>
 
