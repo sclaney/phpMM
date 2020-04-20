949eb482eb48613ade6321addb65ee797716025a
Sam <9093730+sclaney@users.noreply.github.com>
2020-04-20 12:01:50 -0500

    First commit

---
+++ scripts/sayHelloWeb.php
@@ -0,0 +1,14 @@
+<html>
+
+  <head></head>
+
+  <body>
+
+    <h1>PHP and MySQL</h1>
+    <h2>Example 2-1</h2>
+    <h1>Hello, <?php echo $_REQUEST['name']; ?></h1>
+    <p>Great you meet you. Welcome to the beginning of your PHP programming odyssey.</p>
+
+  </body>
+
+</html>
