447b43058be7b06f988bd055f0353eada0e9fea2
Sam <9093730+sclaney@users.noreply.github.com>
2020-04-20 15:36:59 -0500

    requested php data in variables instead of calling it over and over

--- scripts/getFormInfo.php
+++ scripts/getFormInfo.php
@@ -1,16 +1,24 @@
+<?php
+  $first_name = $_REQUEST['first_name'];
+  $last_name = $_REQUEST['last_name'];
+  $email = $_REQUEST['email'];
+  $facebook_url = $_REQUEST['facebook_url'];
+  $twitter_handle = $_REQUEST['twitter_handle'];
+?>
+
 <html>
   <body>
 
     <h1>Example 3-1</h1>
-    
+
     <p>here's a record of what information you submitted:</p>
 
     <p>
-      First name: <?php echo $_REQUEST['first_name']; ?><br />
-      Last name: <?php echo $_REQUEST['last_name']; ?><br />
-      Email: <?php echo $_REQUEST['email']; ?><br />
-      Facebook URL: <?php echo $_REQUEST['facebook_url']; ?><br />
-      Twitter Handle: <?php echo $_REQUEST['twitter_handle']; ?><br />
+      First name: <?php echo $first_name; ?><br />
+      Last name: <?php echo $last_name; ?><br />
+      Email: <?php echo $email; ?><br />
+      Facebook URL: <?php echo $facebook_url; ?><br />
+      Twitter Handle: <?php echo $twitter_handle; ?><br />
     </p>
 
   </body>
