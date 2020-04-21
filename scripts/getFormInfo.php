ae29bdf2ac90ec312053969f865b9de0d1173168
Sam <9093730+sclaney@users.noreply.github.com>
2020-04-20 19:13:56 -0500

    remove whitespace

--- scripts/getFormInfo.php
+++ scripts/getFormInfo.php
@@ -1,13 +1,14 @@
 <?php
-  $first_name = $_REQUEST['first_name'];
-  $last_name = $_REQUEST['last_name'];
-  $email = $_REQUEST['email'];
-  $facebook_url = $_REQUEST['facebook_url'];
+  $first_name = trim($_REQUEST['first_name']);
+  $last_name = trim($_REQUEST['last_name']);
+  $email = trim($_REQUEST['email']);
+  $facebook_url = str_replace("facebook.org", "facebook.com",
+                              trim($_REQUEST['facebook_url']));
   $position = strpos($facebook_url, "facebook.com");
   if ($position === false) {
     $facebook_url = "https://facebook.com/" . $facebook_url;
   }
-  $twitter_handle = $_REQUEST['twitter_handle'];
+  $twitter_handle = trim($_REQUEST['twitter_handle']);
   $twitter_url = "https://twitter.com/";
   $position = strpos($twitter_handle, "@");
   if ($position === false) {
